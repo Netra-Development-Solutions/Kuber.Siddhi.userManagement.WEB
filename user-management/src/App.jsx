0fec93db47270c9d929339307197396419fc513e
Naman Khater <naman22khater@gmail.com>
2023-08-30 00:53:24 +0530

    login form added

--- user-management/src/App.jsx
+++ user-management/src/App.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import RegisterForm from "./components/forms/Register";
+import LoginForm from "./components/forms/Login";
 
 const App = () => (
   <div className="container">
     <RegisterForm />
+    <LoginForm />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
